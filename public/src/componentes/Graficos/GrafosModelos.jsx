2218afbb4bab5356c93da2e01d43ff2523e47667
Alejandro Front <desarrollo@qsocialnow.com>
2024-01-07 12:09:50 -0300

    modelos nuevos

--- public/src/componentes/Graficos/GrafosModelos.jsx
+++ public/src/componentes/Graficos/GrafosModelos.jsx
@@ -49,7 +49,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
       <div className='titulo-modelo'>Sentimientos</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Sentimientos-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Sentimientos-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={sentimientos} className='imagen-grafo-modelos' />
   </div>
@@ -60,7 +60,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Atributos</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Atributos-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Atributos-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={atributospersonalidad} className='imagen-grafo-modelos' />
   </div>
@@ -70,7 +70,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Clima Social</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Clima%20social-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Clima%20social-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={atributospoliticos} className='imagen-grafo-modelos' />
   </div>
@@ -80,7 +80,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Continuidad y cambio</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Continuidad%20y%20cambio-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Continuidad%20y%20cambio-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={contcambio} className='imagen-grafo-modelos' />
   </div>
@@ -91,7 +91,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Emociones Básicas (Plutchik)</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Emociones%20Básicas%20(Plutchik)-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Emociones%20Básicas%20(Plutchik)-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={emocionesbasicas} className='imagen-grafo-modelos' />
   </div>
@@ -101,7 +101,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Preocupaciones</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Preocupaciones-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Preocupaciones-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={preocupaciones} className='imagen-grafo-modelos' />
   </div>
@@ -111,7 +111,7 @@ export default function GrafosModelos(){
   <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Red motivacional del voto</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Red%20motivacional%20del%20voto-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Red%20motivacional%20del%20voto-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={redmotivacional} className='imagen-grafo-modelos' />
   </div>
@@ -121,7 +121,7 @@ export default function GrafosModelos(){
   {/* <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Voto Emocional y Racional</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Voto%20Emocional%20y%20Racional-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Voto%20Emocional%20y%20Racional-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={votoemrac} className='imagen-grafo-modelos' />
   </div>
@@ -132,7 +132,7 @@ export default function GrafosModelos(){
   {/* <div className='grafo-video-modelos'>
   <Tooltip title="Click para ver el grafo">
   <div className='titulo-modelo'>Preocupaciones VEN</div>
-  <a href={`https://qsngrafos.vercel.app/modelos/movilizacion/grafo_modelo-Preocupaciones%20-%20Ven-${filtroFecha}.html`} target="_blank">
+  <a href={`https://qsngrafos.vercel.app/modelos/milei-otros/grafo_modelo-Preocupaciones%20-%20Ven-${filtroFecha}.html`} target="_blank">
   <div className=' cartaGrafo-modelos'>
     <img src={preocupacionesVen} className='imagen-grafo-modelos'/>
   </div>
